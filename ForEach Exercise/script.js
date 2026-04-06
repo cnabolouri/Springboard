565597d7f92afd0ce1150bce7d15eabb17021130
Sina Bolouri <cnabolouri@gmail.com>
2026-04-05 23:05:11 -0700

    MFFFF_2

--- ForEach Exercise/script.js
+++ ForEach Exercise/script.js
@@ -6,6 +6,7 @@ function doubleValues(arr) {
   });
   return result;
 }
+console.log(doubleValues([1, 2, 3])); // [2, 4, 6]
 
 // Only Even Values
 function onlyEvenValues(arr) {
@@ -17,6 +18,7 @@ function onlyEvenValues(arr) {
   });
   return result;
 }
+console.log(onlyEvenValues([1, 2, 3, 4])); // [2, 4]
 
 // Show First And Last
 function showFirstAndLast(arr) {
@@ -26,15 +28,22 @@ function showFirstAndLast(arr) {
   });
   return result;
 }
+console.log(showFirstAndLast(["colt", "matt", "tim", "udemy"])); // ["ct", "mt", "tm", "uy"]
 
 // Add Key And Value
-function showFirstAndLast(arr) {
-  const result = [];
-  arr.forEach((str) => {
-    result.push(str[0] + str[str.length - 1]);
+function addKeyAndValue(arr, key, value) {
+  arr.forEach((obj) => {
+    obj[key] = value;
   });
-  return result;
+  return arr;
 }
+console.log(
+  addKeyAndValue(
+    [{ name: "Elie" }, { name: "Tim" }, { name: "Matt" }, { name: "Colt" }],
+    "title",
+    "Instructor",
+  ),
+);
 
 // Vowel Count
 function vowelCount(str) {
@@ -52,18 +61,32 @@ function vowelCount(str) {
 
   return result;
 }
+console.log(vowelCount("Elie")); // {e: 2, i: 1}
 
 // Double Values With Map
 function doubleValuesWithMap(arr) {
   return arr.map((val) => val * 2);
 }
+console.log(doubleValuesWithMap([1, 2, 3])); // [2, 4, 6]
 
 // Exact Key
 function extractKey(arr, key) {
   return arr.map((obj) => obj[key]);
 }
+console.log(
+  extractKey(
+    [{ name: "Elie" }, { name: "Tim" }, { name: "Matt" }, { name: "Colt" }],
+    "name",
+  ),
+); // ["Elie", "Tim", "Matt", "Colt"]
 
 // Filter By Value
 function filterByValue(arr, key) {
   return arr.filter((obj) => obj[key] !== undefined);
 }
+console.log(
+  filterByValue(
+    [{ name: "Elie" }, { name: "Tim" }, { name: "Matt" }, { name: "Colt" }],
+    "name",
+  ),
+); // [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
